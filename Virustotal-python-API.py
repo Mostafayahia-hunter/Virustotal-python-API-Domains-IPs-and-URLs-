382a019350806095342deab0788834495e59546c
Mostafayahia-hunter <76810309+Mostafayahia-hunter@users.noreply.github.com>
2021-01-07 12:32:11 +0200

    fix the IF Condition to resolve the non exist urls
    if json_response['response_code'] <= 0:
            with open('not Found result.txt', 'a')  as notfound:
                notfound.write(i) and notfound.write("\tNOT found please Scan it manually\n")
        elif json_response['response_code'] >= 1:
    
            if json_response['positives'] <= 0:
                with open('Virustotal Clean result.txt', 'a')  as clean:
                    clean.write(i) and clean.write("\tNOT malicious\n")
            else:
                with open('Virustotal Malicious result.txt', 'a')  as malicious:
                    malicious.write(i) and malicious.write("\t Malicious\n")

--- Virustotal-python-API.py
+++ Virustotal-python-API.py
@@ -9,20 +9,33 @@ domain_CSV = pandas.read_csv((file_path))
 
 Urls = domain_CSV['Domain'].tolist()
 
-API_key = 'yourAPIkey'
+API_key = 'YOURAPIKEY'
 url = 'https://www.virustotal.com/vtapi/v2/url/report'
+
+
+parameters = {'apikey': API_key, 'resource': Urls}
+
+response= requests.get(url=url, params=parameters)
+json_response= json.loads(response.text)
+
+
 
 for i in Urls:
     parameters = {'apikey': API_key, 'resource': i}
 
     response= requests.get(url=url, params=parameters)
     json_response= json.loads(response.text)
-    if json_response['positives'] <= 0:
-        with open('Virustotal Clean result.txt', 'a')  as clean:
-            clean.write(i) and clean.write("\tNOT malicious\n")
-    else:
-        with open('Virustotal Malicious result.txt', 'a')  as malicious:
-            malicious.write(i) and malicious.write("\t Malicious\n")
+    
+    if json_response['response_code'] <= 0:
+        with open('not Found result.txt', 'a')  as notfound:
+            notfound.write(i) and notfound.write("\tNOT found please Scan it manually\n")
+    elif json_response['response_code'] >= 1:
+
+        if json_response['positives'] <= 0:
+            with open('Virustotal Clean result.txt', 'a')  as clean:
+                clean.write(i) and clean.write("\tNOT malicious\n")
+        else:
+            with open('Virustotal Malicious result.txt', 'a')  as malicious:
+                malicious.write(i) and malicious.write("\t Malicious\n")
 
     time.sleep(15)
-
