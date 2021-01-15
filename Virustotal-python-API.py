9f23dbd32b51f6c6003eaea2f918affefbab2471
Mostafayahia-hunter <76810309+Mostafayahia-hunter@users.noreply.github.com>
2021-01-15 18:12:21 +0200

    Update Virustotal-python-API.py

--- Virustotal-python-API.py
+++ Virustotal-python-API.py
@@ -9,7 +9,7 @@ domain_CSV = pandas.read_csv((file_path))
 
 Urls = domain_CSV['Domain'].tolist()
 
-API_key = 'yourAPIkey'
+API_key = 'YourAPIkey'
 url = 'https://www.virustotal.com/vtapi/v2/url/report'
 
 
@@ -17,8 +17,6 @@ parameters = {'apikey': API_key, 'resource': Urls}
 
 response= requests.get(url=url, params=parameters)
 json_response= json.loads(response.text)
-
-
 
 for i in Urls:
     parameters = {'apikey': API_key, 'resource': i}
@@ -33,7 +31,7 @@ for i in Urls:
 
         if json_response['positives'] <= 0:
             with open('Virustotal Clean result.txt', 'a')  as clean:
-                clean.write(i) and clean.write("\tNOT malicious\n")
+                clean.write(i) and clean.write("\t NOT malicious \n")
         else:
             with open('Virustotal Malicious result.txt', 'a')  as malicious:
                 malicious.write(i) and malicious.write("\t Malicious") and malicious.write("\t this Domains Detectd by   "+ str(json_response['positives']) + "  Solutions\n")
